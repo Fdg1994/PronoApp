252780175a776a2a5e4c963f449cfd74571871ac
e <frederik.de.gottal@gmail.com>
2023-01-31 17:37:44 +0100

    added cors support to api

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,4 @@
 using API.Extensions;
-using API.Interface;
-using API.Services;
-using Data.Interfaces;
-using Infrastructure.Data;
-using Infrastructure.Data.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +11,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
